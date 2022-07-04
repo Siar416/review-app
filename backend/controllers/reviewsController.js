d1f63adea6aa60f520bdcf1ec728a478132acc02
Siar Wahidi <siar.wahidi@gmail.com>
2022-07-04 13:02:41 -0400

    created route to get single review

--- backend/controllers/reviewsController.js
+++ backend/controllers/reviewsController.js
@@ -12,6 +12,26 @@ const getReviews = async (req, res) => {
 };
 
 // GET single review
+const getReview = async (req, res) => {
+  const { id } = req.params;
+
+  // checking if the id is valid
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such product" });
+  }
+
+  try {
+    const review = await Review.findById(id);
+
+    if (!review) {
+      return res.status(404).json({ error: "No such product" });
+    }
+
+    res.status(200).json(review);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
   getReviews,
