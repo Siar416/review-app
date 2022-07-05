5ef81608770c9be1709e5b102591447db12fa5b1
Siar Wahidi <siar.wahidi@gmail.com>
2022-07-05 12:43:33 -0400

    added two button to home page to goto different links

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,31 +1,43 @@
 import { useEffect } from "react";
 import ReviewDetails from "../components/ReviewDetails";
 import { useReviewContext } from "../hooks/useReviewContext";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const { reviews, dispatch } = useReviewContext();
 
-  useEffect(() => {
-    const fetchReviews = async () => {
-      const response = await fetch("/api/reviews");
-      const json = await response.json();
+  // useEffect(() => {
+  //   const fetchReviews = async () => {
+  //     const response = await fetch("/api/reviews");
+  //     const json = await response.json();
 
-      if (response.ok) {
-        dispatch({
-          type: "GET_REVIEWS",
-          payload: json,
-        });
-      }
-    };
-    fetchReviews();
-  }, [dispatch]);
+  //     if (response.ok) {
+  //       dispatch({
+  //         type: "GET_REVIEWS",
+  //         payload: json,
+  //       });
+  //     }
+  //   };
+  //   fetchReviews();
+  // }, [dispatch]);
+
+  // return (
+  //   <div className="reviews-container">
+  //     {reviews &&
+  //       reviews.map((review) => (
+  //         <ReviewDetails key={review._id} review={review} />
+  //       ))}
+  //   </div>
+  // );
 
   return (
-    <div className="reviews-container">
-      {reviews &&
-        reviews.map((review) => (
-          <ReviewDetails key={review._id} review={review} />
-        ))}
+    <div className="main-page">
+      <Link className="link-botton" to="/reviews-list">
+        List Reviews
+      </Link>
+      <Link className="link-botton" to="/create-review">
+        Create Reviews
+      </Link>
     </div>
   );
 };
