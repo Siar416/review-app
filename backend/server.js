202b566ecc54be07b7875fe426c0c34085e73ef6
Siar Wahidi <siar.wahidi@gmail.com>
2022-07-05 12:19:21 -0400

    created a setup proxy file to fix issue

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const reviewRoutes = require("./routes/reviews");
 
 // express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
